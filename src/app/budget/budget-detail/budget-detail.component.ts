9905b0a75f6c2c35155eee3db260d40477c204ad
MarSax <msassanelli12@gmail.com>
2020-11-15 20:25:34 +0100

    project setup v4

--- src/app/budget/budget-detail/budget-detail.component.ts
+++ src/app/budget/budget-detail/budget-detail.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Budget} from '../budget';
 import {NgbModal, NgbModalConfig} from '@ng-bootstrap/ng-bootstrap';
+import {BudgetService} from '../budget.service';
 
 @Component({
   selector: 'app-budget-detail',
@@ -12,7 +13,7 @@ export class BudgetDetailComponent implements OnInit {
   @Input() incomeBudget: Budget;
   @Input() expenseBudget: Budget;
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private budgetService: BudgetService) { }
 
   ngOnInit(): void {
   }
@@ -21,4 +22,7 @@ export class BudgetDetailComponent implements OnInit {
     this.modalService.open(content, { centered: true });
   }
 
+  public delete(id: number): void {
+    this.budgetService.delete(id).subscribe();
+  }
 }
